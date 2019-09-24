812d86e04e7d73cf0aecdcf210274c0f45fc5c6e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-24 22:25:39 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,6 +20,10 @@ class Waiter
     end
   end
   
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
